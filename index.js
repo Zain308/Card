a631c5a9c05e180b9301fe3d53a106d4b584d11b
M.Zain ul abideen <140058199+Zain308@users.noreply.github.com>
2025-03-01 23:33:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var b=document.querySelectorAll("button")[1];
+b.addEventListener("click",function(){
+   var a= document.querySelector("h5");
+   a.innerHTML="Friends"
+});
+
+
+
+var c=document.querySelectorAll("button")[0];
+c.addEventListener("click",function(){
+   var d= document.querySelector("h5");
+   d.innerHTML="Stranger"
+});
